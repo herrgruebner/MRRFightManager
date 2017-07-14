de2d00fd79a248023fc57268001081f4b6a33dab
herr gruebner <maxmanrules@gmail.com>
2017-07-15 06:14:19 +1200

    UI update

--- Basicfightmanager/Scripts/signalRFightManagement.js
+++ Basicfightmanager/Scripts/signalRFightManagement.js
@@ -12,20 +12,25 @@ manager.client.stopTimer = function () {
 manager.client.updateRobotNames = function (name1, name2) {
     $('#robot1Name').text(name1);
     $('#robot2Name').text(name2);
+    $('.result').addClass('hidden');
+    $('#competitors').removeClass('hidden');
 }
 
 manager.client.showOutcome = function (winningRobot, losingRobot, winType) {
     $('.result #winningRobot').text(winningRobot);
     $('.result #losingRobot').text(losingRobot);
     if (winType = 'Tie') {
-        $('.result #winType').text('Tied With')
+        $('.result #winType').text('tied with')
     }
     else if (winType = 'Knockout') {
-        $('.result #winType').text('Knocked out')
+        $('.result #winType').text('knocked out')
     }
     else if (winType = 'Tie') {
-        $('.result #winType').text('Beat')
+        $('.result #winType').text('beat')
     }
+    $('#competitors').addClass('hidden');
+    $('.result').removeClass('hidden');
+
 }
 function startClock() {
     manager.server.startTimer();
@@ -39,6 +44,7 @@ function updateRobotNames() {
     var name1 = $('#robot1Name').val();
     var name2 = $('#robot2Name').val();
     manager.server.updateRobotNames(name1, name2);
+    
 }
 
 function queueFight(row) {
